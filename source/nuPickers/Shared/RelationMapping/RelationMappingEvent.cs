3a385b04702a20e3a64ffe5acd0bd761a8fed49e
sidesixmedia <cbell@sidesixmedia.com>
2018-01-30 16:58:28 -0600

    Moved relationOnly value changes to SAVING events instead of SAVE so the nullified values will be saved naturally as a part of the current save routine.

--- source/nuPickers/Shared/RelationMapping/RelationMappingEvent.cs
+++ source/nuPickers/Shared/RelationMapping/RelationMappingEvent.cs
@@ -14,40 +14,53 @@
 	/// </summary>
 	public class RelationMappingEvent : ApplicationEventHandler
     {
+		
+		protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+		{
 
-		// S6 Saves a placeholder value when the 
-		//private const string RELATIONS_ONLY_TOKEN = "relationsOnlyToken";
-
-        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
-        {
-            ContentService.Saved += this.ContentService_Saved;
-            MediaService.Saved += this.MediaService_Saved;
-            MemberService.Saved += this.MemberService_Saved;
-
-			// S6 Added
 			ContentService.Saving += this.ContentService_Saving;
-            
-            // NOTE: all relations to an id are automatically deleted when emptying the recycle bin
-        }
+			MediaService.Saving += this.MediaService_Saving;
+			MemberService.Saving += this.MemberService_Saving;
+			
+			// NOTE: all relations to an id are automatically deleted when emptying the recycle bin
+		}
 
-		// S6
 		private void ContentService_Saving(IContentService sender, SaveEventArgs<IContent> e)
 		{
-			
-			foreach (IContent savedEntity in e.SavedEntities)
+			this.Saving((IService)sender, e.SavedEntities);
+		}
+
+        private void MediaService_Saving(IMediaService sender, SaveEventArgs<IMedia> e)
+        {
+            this.Saving((IService)sender, e.SavedEntities);
+        }
+
+        private void MemberService_Saving(IMemberService sender, SaveEventArgs<IMember> e)
+        {
+            this.Saving((IService)sender, e.SavedEntities);
+        }
+
+        /// <summary>
+        /// Create/update any relations for pickers and nullify their stored values in RelationsOnly is the selected saveFormat
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="changedEntities"></param>
+        private void Saving(IService sender, IEnumerable<IContentBase> changedEntities)
+        {
+			foreach (IContent entity in changedEntities)
 			{
-				if(savedEntity != null)
+				if (entity != null)
 				{
 					// Loop all Pickers in saved Content item
-					foreach (PropertyType propertyType in savedEntity.PropertyTypes.Where(x => PickerPropertyValueConverter.IsPicker(x.PropertyEditorAlias)))
+					foreach (PropertyType propertyType in entity.PropertyTypes.Where(x => PickerPropertyValueConverter.IsPicker(x.PropertyEditorAlias)))
 					{
 						Picker picker = new Picker(
-										   savedEntity.Id,
-										   savedEntity.ParentId,
+										   entity.Id,
+										   entity.ParentId,
 										   propertyType.Alias,
 										   propertyType.DataTypeDefinitionId,
 										   propertyType.PropertyEditorAlias,
-										   savedEntity.GetValue(propertyType.Alias));
+										   entity.GetValue(propertyType.Alias));
 
 						if (!string.IsNullOrWhiteSpace(picker.RelationTypeAlias))
 						{
@@ -55,96 +68,33 @@
 
 							if (isRelationsOnly)
 							{
-								//savedEntity.SetValue(propertyType.Alias, "s6RelationsOnly");								
+								if (picker.SavedValue == null)
+								{
+									picker.PickedKeys = new string[] { };
+								}
+								else
+								{
+									// manually set on picker obj, so it doesn't then attempt to read picked keys from the database
+									picker.PickedKeys = SaveFormat.GetKeys(picker.SavedValue.ToString()).ToArray();
+
+									// delete saved value (setting it to null)
+									entity.SetValue(propertyType.Alias, null);
+								}
+
 							}
+
+							// update database
+							RelationMapping.UpdateRelationMapping(
+								picker.ContextId, // savedEntity.Id
+								picker.PropertyAlias, // propertyType.Alias
+								picker.RelationTypeAlias,
+								isRelationsOnly,
+								picker.PickedIds.ToArray());
+
 						}
-					}					
-				}				
+					}
+				}
 			}
 		}
-
-		private void ContentService_Saved(IContentService sender, SaveEventArgs<IContent> e)
-        {
-            this.Saved((IService)sender, e.SavedEntities);
-        }
-
-        private void MediaService_Saved(IMediaService sender, SaveEventArgs<IMedia> e)
-        {
-            this.Saved((IService)sender, e.SavedEntities);
-        }
-
-        private void MemberService_Saved(IMemberService sender, SaveEventArgs<IMember> e)
-        {
-            this.Saved((IService)sender, e.SavedEntities);
-        }
-
-        /// <summary>
-        /// combined event for content / media / member 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="savedEntities"></param>
-        private void Saved(IService sender, IEnumerable<IContentBase> savedEntities)
-        {
-            foreach (IContentBase savedEntity in savedEntities)
-            {
-                // for each property
-                foreach (PropertyType propertyType in savedEntity.PropertyTypes.Where(x => PickerPropertyValueConverter.IsPicker(x.PropertyEditorAlias)))
-                {
-                    // create picker supplying all values
-                    Picker picker = new Picker(
-                                            savedEntity.Id, 
-                                            savedEntity.ParentId,
-                                            propertyType.Alias, 
-                                            propertyType.DataTypeDefinitionId, 
-                                            propertyType.PropertyEditorAlias,
-                                            savedEntity.GetValue(propertyType.Alias));
-
-                    if (!string.IsNullOrWhiteSpace(picker.RelationTypeAlias))
-                    {
-                        bool isRelationsOnly = picker.GetDataTypePreValue("saveFormat").Value == "relationsOnly";
-
-                        if (isRelationsOnly) 
-                        {
-
-							// S6 TODO If Relations Only is true, always save a placeholder value to work-around Umbraco unpublishing
-														
-                            if (picker.SavedValue == null)
-                            {
-                                picker.PickedKeys = new string[] { };
-                            }
-                            else
-                            {
-                                // manually set on picker obj, so it doesn't then attempt to read picked keys from the database
-                                picker.PickedKeys = SaveFormat.GetKeys(picker.SavedValue.ToString()).ToArray();
-
-								// delete saved value (setting it to null)
-								//savedEntity.SetValue(propertyType.Alias, null); // S6 No, changing a property value here is what marks the Content as Dirty and is likely where the Publishing bug originates
-								
-								if (sender is IContentService)
-                                {
-                                    ((IContentService)sender).Save((IContent)savedEntity, 0, false);
-                                }
-                                else if (sender is IMediaService)
-                                {
-                                    ((IMediaService)sender).Save((IMedia)savedEntity, 0, false);
-                                }
-                                else if (sender is IMemberService)
-                                {
-                                    ((IMemberService)sender).Save((IMember)savedEntity, false);
-                                }
-                            }
-                        }
-                       
-                        // update database
-                        RelationMapping.UpdateRelationMapping(
-                                                picker.ContextId,           // savedEntity.Id
-                                                picker.PropertyAlias,       // propertyType.Alias
-                                                picker.RelationTypeAlias,
-                                                isRelationsOnly,
-                                                picker.PickedIds.ToArray());
-                    }
-                }
-            }
-        }
     }
 }
